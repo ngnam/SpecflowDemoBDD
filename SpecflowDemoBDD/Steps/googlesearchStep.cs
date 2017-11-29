17c1a715dac92875b68ed8220678c50be2e3ea45
ngnam <nguyenvannam0411@gmail.com>
2017-11-29 14:17:37 +0700

    update coide

--- SpecflowDemoBDD/Steps/googlesearchStep.cs
+++ SpecflowDemoBDD/Steps/googlesearchStep.cs
@@ -1,36 +1,45 @@
-﻿using System;
+﻿using Baseclass.Contrib.SpecFlow.Selenium.NUnit.Bindings;
+using OpenQA.Selenium;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
 
 namespace SpecflowDemoBDD.Steps
 {
     [Binding]
     public class googlesearchStep
     {
+        IWebDriver currentDriver = null;
         [Given(@"I have navigated to Google page")]
         public void GivenIHaveNavigatedToGooglePage()
         {
-            ScenarioContext.Current.Pending();
+            Browser.Current.Navigate().GoToUrl(ConfigurationManager.AppSettings["seleniumBaseUrl"]);
+            currentDriver = Browser.Current;
         }
 
         [Given(@"I see the google page fully loaded")]
         public void GivenISeeTheGooglePageFullyLoaded()
         {
-            ScenarioContext.Current.Pending();
+            if (currentDriver.FindElement(By.Name("q")).Displayed)
+                Console.WriteLine("Page loaded fully");
+            else Console.WriteLine("Page failded to loaded");
         }
 
         [When(@"I type search keyword as")]
         public void WhenITypeSearchKeywordAs(Table table)
         {
-            ScenarioContext.Current.Pending();
+            dynamic tableDetail = table.CreateDynamicInstance();
+            currentDriver.FindElement(By.Name("q")).SendKeys(tableDetail.keyword);
         }
 
         [Then(@"I should see the result for keyword")]
         public void ThenIShouldSeeTheResultForKeyword(Table table)
         {
-            ScenarioContext.Current.Pending();
+           // ScenarioContext.Current.Pending();
         }
 
     }
